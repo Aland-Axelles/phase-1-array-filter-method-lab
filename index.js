8b71a0ec7cff31b278fc36cadf2af8659f8a91a2
aland <rebwar.aland11@gmail.com>
2024-04-07 02:21:21 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function matchName(drivers, name) {
+    return drivers.filter(driver =>
+      driver.name.toLowerCase() === name.toLowerCase()
+    );
+  }
+
+  
+  function fuzzyMatch(drivers, prefix) {
+    return drivers.filter(driver =>
+      driver.toLowerCase().startsWith(prefix.toLowerCase())
+    );
+  }
+
+  
+  function findMatching(drivers, query) {
+    return drivers.filter(driver =>
+      driver.toLowerCase() === query.toLowerCase()
+    );
+  }
+  
